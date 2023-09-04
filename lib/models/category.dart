0d9f1afff4b45b95c86a7010e0255570ba225558
Pkisic <pedjakisic@gmail.com>
2023-09-04 15:23:59 +0200

    [Diplomski] Add category feature # Add category Model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:diplomski/services/notes_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,16 +9,16 @@ class Category {
   final Color color;
   final String? description;
 
-  Category(
-    this.description, {
+  Category({
     required this.id,
     required this.name,
     required this.color,
+    required this.description,
   });
 
   Category.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         name = map[nameColumn] as String,
         description = map[descriptionColumn] as String,
-        color = map[colorColumn] as Color;
+        color = Color(map[colorColumn] as int);
 }
